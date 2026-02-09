d0d87246e3a7dcdec442eea9e0fb8c3edf51fbeb
mbrink <brinkschmidt.malte@gmail.com>
2026-02-09 15:43:45 +0100

    updated db with picture functionality

---
+++ pb_migrations/1770589397_updated_quotes.js
@@ -0,0 +1,54 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("4lc1212flbtk3g5")
+
+  // remove
+  collection.schema.removeField("t00oxq6h")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "pwqrtnyl",
+    "name": "authorpic",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "46tu3u6bfyfuk3u",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("4lc1212flbtk3g5")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "t00oxq6h",
+    "name": "headshot",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [],
+      "thumbs": [],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  // remove
+  collection.schema.removeField("pwqrtnyl")
+
+  return dao.saveCollection(collection)
+})
